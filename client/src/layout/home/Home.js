9e0a282fbfda99dff34a85ca0c85b54d5d678d20
Ansah1234 <ansahpeter123@yaoo.com>
2021-02-07 09:13:40 +0100

    aftersteps

--- client/src/layout/home/Home.js
+++ client/src/layout/home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Landing from './components/Landing';
 import Steps from './components/Steps'
+import Aftersteps from './components/Aftersteps'
 
 const Home = () => {
 	return (
 		<>
 			<Landing></Landing>
 			<Steps></Steps>
+			<Aftersteps></Aftersteps>
 		</>
 	);
 };
---
+++ client/src/layout/home/components/Aftersteps.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core'
+
+const useStyles=makeStyles({
+    root:{
+        
+    }
+})
+
+const Aftersteps = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Aftersteps

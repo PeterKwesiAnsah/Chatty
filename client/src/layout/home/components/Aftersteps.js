b27c6e4bd4267ea0bc6cc7b69afbb4acfb4f92d9
Ansah1234 <ansahpeter123@yaoo.com>
2021-02-07 09:22:28 +0100

    lottie nserted

--- client/src/layout/home/components/Aftersteps.js
+++ client/src/layout/home/components/Aftersteps.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import {makeStyles} from '@material-ui/core'
+import React from 'react';
+import { makeStyles, Grid } from '@material-ui/core';
+import animationData from '../../../lotties/growthAnimation.json';
+import Lottie from 'react-lottie';
 
-const useStyles=makeStyles({
-    root:{
-        
-    }
-})
+const useStyles = makeStyles({
+	root: {
+		width: '100%',
+	},
+});
+
+const defaultOptions = {
+	loop: true,
+	autoplay: true,
+	animationData: animationData,
+	rendererSettings: {
+		preserveAspectRatio: 'xMidYMid slice',
+	},
+};
 
 const Aftersteps = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<Grid container alignItems="center">
+				<Grid item md={6} lg={6}></Grid>
+				<Grid item md={6} lg={6}>
+					<Lottie options={defaultOptions} height={450} width={550} />
+				</Grid>
+			</Grid>
+		</div>
+	);
+};
 
-export default Aftersteps
+export default Aftersteps;

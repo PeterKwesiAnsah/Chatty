03ddc607c309e717fdcb96fadd02f6b83500458e
Ansah1234 <ansahpeter123@yaoo.com>
2021-02-07 10:38:29 +0100

    footer

---
+++ client/src/layout/home/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {makeStyles} from '@material-ui/core'
+
+const useStyles=makeStyles({
+    
+})
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer

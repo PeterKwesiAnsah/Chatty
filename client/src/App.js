b60bf1a281d1dec5905666e1e3b07988576fc754
Ansah1234 <ansahpeter123@yaoo.com>
2021-02-10 11:15:44 +0100

    declaring routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import Home from './layout/home/Home';
 import { ThemeProvider, responsiveFontSizes } from '@material-ui/core/styles';
 import GlobalStyles from './GlobalStyles';
 import theme from './theme/theme.js';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
 	return (
 		<ThemeProvider theme={responsiveFontSizes(theme)}>
 			<GlobalStyles></GlobalStyles>
-			<>
-				<Home></Home>
-			</>
+			<Router>
+				<Switch>
+					<Route path="/" exact>
+						<Home></Home>
+					</Route>
+				</Switch>
+			</Router>
 		</ThemeProvider>
 	);
 }

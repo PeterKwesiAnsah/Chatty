e78c127cbfcbbc60b01351eaed1031e3ad7c40fd
Ansah1234 <ansahpeter123@yaoo.com>
2021-03-15 19:48:31 +0100

    make it better

--- server/server.js
+++ server/server.js
@@ -30,6 +30,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-	console.log(` Server ready at ${url}`);
-});
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+	console.log(`🚀 Server ready at ${url}`);
+  });

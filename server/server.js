c9ebf0e9bcafb2082bee1017ddd3bbf4345b6c74
Ansah1234 <ansahpeter123@yaoo.com>
2021-03-16 22:10:05 +0100

    share bug fixed

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const models = require('./models/Models.js');
 
 //connect to database
 connect();
-    
+
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
@@ -16,7 +16,7 @@ const server = new ApolloServer({
 	context: async ({ req, connection }) => {
 		const token =
 			req?.headers.authorization || connection.context.authorization || null;
-			console.log(token)
+
 		try {
 			if (token) {
 				const user = await getUserFromToken(token, models.User);
@@ -25,7 +25,7 @@ const server = new ApolloServer({
 			}
 			return { createToken, getUserFromToken, models };
 		} catch (e) {
-			console.log(e);   
+			console.log(e);
 		}
 	},
 });
@@ -33,4 +33,4 @@ const server = new ApolloServer({
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
 	console.log(`🚀 Server ready at ${url}`);
-  });
+});
